842340e1e245571174c8e650474c7cc8e6244cae
Ryan Harvey <ryanhmn@gmail.com>
2021-11-06 16:38:27 -0500

    adds webview to tree view

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,160 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+	const testProvider = new TestsViewProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(TestsViewProvider.viewType, testProvider));
+
+	console.log('Test');
+	context.subscriptions.push(
+		//This doesn't matter??
+		vscode.commands.registerCommand('stogitresponse.addTest', () => {
+			console.log('Add test button pushed');
+			vscode.window.showInformationMessage('Add Test Button Pushed');
+			//testProvider.addTest();
+		}));
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('stogitresponse.addTest', () => {
+	let disposable = vscode.commands.registerCommand('stogitresponse.myTest', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Successfully added test!');
+		vscode.window.showInputBox({
+			title: "Add Test",
+			placeHolder: "Test Name"
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+class TestsViewProvider implements vscode.WebviewViewProvider {
+
+	public static readonly viewType = 'stogitresponse-createtests';
+
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+
+			localResourceRoots: [
+				this._extensionUri
+			]
+		};
+
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+		webviewView.webview.onDidReceiveMessage(data => {
+			switch (data.type) {
+				case 'colorSelected':
+					{
+						vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+						break;
+					}
+				//3?
+				case 'addTest':
+					{
+						vscode.window.showInformationMessage('Add test button pushed!');
+						break;
+					}
+				case 'test':
+					{
+						vscode.window.showInformationMessage('class exists');
+						console.log('class exists');
+						break;
+					}
+			}
+		});
+	}
+
+	// public addColor() {
+	// 	if (this._view) {
+	// 		this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+	// 		this._view.webview.postMessage({ type: 'addColor' });
+	// 	}
+	// }
+
+	// public clearColors() {
+	// 	if (this._view) {
+	// 		this._view.webview.postMessage({ type: 'clearColors' });
+	// 	}
+	// }
+
+	// public addTest() {
+	// 	if (this._view) {
+	// 		console.log("success");
+	// 		this._view.show?.(true);
+	// 		this._view.webview.postMessage({ type: 'addTest' });
+	// 	} else {
+	// 		console.log("failure");
+	// 	}
+	// }
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+
+		// Do the same for the stylesheet.
+		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
+		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+
+		// Use a nonce to only allow a specific script to be run.
+		const nonce = getNonce();
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+				
+				<title>Cat Colors</title>
+			</head>
+			<body>
+				<input placeholder="Input"></input>
+				<input placeholder="Expected Output"></input>
+				<button class="add-test-button">Add Test</button>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+	}
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
